b362473117d672e2d3d25bc3d00dd5b6f5ac5382
marcush <wasd666@live.com>
2023-09-28 14:47:32 +0800

    add 007

--- uncategorized/007.modulo_and_division.Reverse_Integer.go
+++ uncategorized/007.modulo_and_division.Reverse_Integer.go
@@ -32,3 +32,19 @@ func ReverseInteger(number int) (res int) {
 	}
 	return -res
 }
+
+// AI generated solution:
+func reverse(x int) int {
+	reversed := 0
+
+	for x != 0 {
+		digit := x % 10
+		reversed = reversed*10 + digit
+		x /= 10
+	}
+
+	if reversed > math.MaxInt32 || reversed < math.MaxInt32 {
+		return 0
+	}
+	return reversed
+}
