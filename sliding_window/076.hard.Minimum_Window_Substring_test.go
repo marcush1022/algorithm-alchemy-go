7434e25abf133b624f02cf71067ba9ea33c6a258
marcush <wasd666@live.com>
2024-01-09 12:03:28 +0800

    fix: sliding window

--- sliding_window/076.hard.Minimum_Window_Substring_test.go
+++ sliding_window/076.hard.Minimum_Window_Substring_test.go
@@ -13,5 +13,4 @@ func Test_minWindow(t *testing.T) {
 	assert.Equal(t, "BANC", minWindow("ADOBECODEBANC", "ABC"))
 	assert.Equal(t, "a", minWindow("a", "a"))
 	assert.Equal(t, "", minWindow("a", "aa"))
-
 }
